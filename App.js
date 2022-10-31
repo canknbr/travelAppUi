3432cf941ac6583da39265f2bf42f9e13372e008
Can Kanbur <cankanbur1@gmail.com>
2022-10-31 16:11:19 +0300

    completed onboard screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import HomeScreen from './screens/HomeScreen';
+import OnBoardingScreen from './screens/OnBoardingScreen';
+import DetailsScreen from './screens/DetailsScreen';
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- screens/OnBoardingScreen.js
+++ screens/OnBoardingScreen.js
@@ -1,12 +1,75 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ImageBackground,
+  Pressable,
+} from 'react-native';
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const OnBoardingScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>OnBoardingScreen</Text>
+    <View style={{flex: 1}}>
+      <StatusBar barStyle="light-content" backgroundColor="rgba(0,0,0,0)" />
+      <ImageBackground
+        source={require('../assets//onboardImage.jpg')}
+        style={styles.bgImage}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome to the App</Text>
+          <Text style={{...styles.title, marginTop: 6}}>Let's discover</Text>
+          <Text style={styles.subtitle}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+            maxime perspiciatis exercitationem iusto provident eius modi iure
+            nulla. Laboriosam non amet culpa accusamus voluptatum enim magni
+            eligendi nulla nesciunt voluptatem!
+          </Text>
+          <Pressable
+            onPress={() => navigation.navigate('HomeScreen')}
+            style={styles.btn}>
+            <Text style={styles.textBtn}>Get Started</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  bgImage: {
+    flex: 1,
+  },
+  container: {
+    height: '50%',
+    bottom: 0,
+    position: 'absolute',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'rgba(255,255,255,0.9)',
+    marginTop: 20,
+    lineHeight: 25,
+  },
+  btn: {
+    backgroundColor: 'white',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  textBtn: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'rgba(0,0,0,0.8)',
+  },
+});
 export default OnBoardingScreen;
